340972da06b9d7342a49845c94b3ea1b973ce318
Loris <45939615+lchastanet@users.noreply.github.com>
2022-03-31 14:25:19 +0200

    add html structure & css

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,50 @@
 import "../styles/App.css"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <h1>🏠 Find your home</h1>
+      </header>
+      <div className="wrapper">
+        <div className="filters">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="🔎 Type to search"
+          />
+          <select className="select" name="home">
+            <option value="All">All</option>
+            <option value="Flat">Flat</option>
+            <option value="House">House</option>
+          </select>
+          <div>
+            <label htmlFor="available">Show only Available </label>
+            <input
+              type="checkbox"
+              className="available-checkbox"
+              name="checkbox"
+              id=""
+            />
+          </div>
+        </div>
+        <div className="cards">
+          {/*
+    <div class="card">
+       <div class="card-header">
+          <div class="card-img" style="background-image:url('https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80') "></div>
+        </div>
+      <div class="card-body">
+        <h2 class="card-title">Sample Card</h2>
+        <p class="card-description">This is just a sample card, take the structure of this card as an example for the function createCard</p>
+      <button class="card-button">I want it!</button>
+      </div>
+    </div>
+      </div> */}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
